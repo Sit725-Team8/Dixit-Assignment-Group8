67e0a6a52af90c58e896a6cbd1ba9b07832653e6
Ray Guo <ma2808203259@hotmail.com>
2020-10-19 19:33:01 +0800

    fixed conflit

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 const socket = io.connect('http://localhost:3030')
 
-const joinRoom = document.getElementById('joinRoom');
+const testButton = document.getElementById('socketButton');
 const testInput = document.getElementById('socketInput');
 const testOutput = document.getElementById('socketOutput');
 const calButton = document.getElementById('calculatorBtn');
@@ -10,95 +10,70 @@ const calButton = document.getElementById('calculatorBtn');
 //     userName
 // } from './env.js'
 
-// let session = sessionStorage.getItem('test')
-// console.log(session+ '  is session from client.js');
-
-
-
 socket.on('test-socket', data => {
     var node = document.createElement('Li')
-    var textNode = document.createTextNode(data.card1);
-    node.appendChild(textNode)
-    testOutput.appendChild(node)
-})
-
-socket.on('player', data => {
-    var node = document.createElement('Li')
-    var textNode = document.createTextNode(data);
-    node.appendChild(textNode)
-    testOutput.appendChild(node)
-    console.log(data);
-})
-socket.on('hello', data => {
-    var node = document.createElement('Li')
-    var textNode = document.createTextNode(data);
+    var textNode = document.createTextNode(data.story);
     node.appendChild(textNode)
     testOutput.appendChild(node)
 })
 
 testButton.addEventListener('click', () => {
-            let userId = sessionStorage.getItem('userId')
-            let userName = sessionStorage.getItem('userName')
+    let userId = sessionStorage.getItem('userId')
+    let userName = sessionStorage.getItem('userName')
+    let message = testInput.value;
+    console.log(userId, userName);
+    console.log(message);
+    let payload = {
+        'story': message,
+        'senderId': userId,
+        'senderName': userName
+    }
+    socket.emit('test-socket', payload)
+})
 
-            socket.on('join', data => {
-                console.log('You have connect to Room: ' + data);
-            })
-
-
-            socket.on('addplayer', message => {
-                console.log(message);
-            })
-
-
-            joinRoom.addEventListener('click', () => {
-                const playerName = document.getElementById('inputName').value;
-                let payload = {
-                    'playerName': playerName
-                }
-                socket.emit('join', payload)
-            })
-
-            testButton.addEventListener('click', () => {
-                let message = testInput.value;
-                console.log(userId, userName);
-                console.log(message);
-                let payload = {
-                    'story': message,
-                    'senderId': userId,
-                    'senderName': userName
-                }
-                socket.emit('test-socket', payload)
-            })
-
-            let i = 0;
-            calButton.addEventListener('click', () => {
-                let userId = sessionStorage.getItem('userId')
-                let userName = sessionStorage.getItem('userName')
-                //since the vote function have done use random value
-                let random = Math.floor(Math.random() * 2);
-                let voteResult = (random == 0) ? true : false;
-                //for test propose
-                let payload = {}
-                if (i == 0) {
-                    payload = {
-                        name: userName,
-                        id: userId,
-                        vote: voteResult,
-                        storyTeller: true
-                    }
-                    i++;
-                } else {
-                    payload = {
-                        name: userName,
-                        id: userId,
-                        vote: voteResult,
-                        storyTeller: false
-                    }
-                }
-                console.log(payload);
-                console.log(i);
-                //send the vote data to the server
-                socket.emit('send-result', payload)
-            })
+let i = 0;
+calButton.addEventListener('click', () => {
+    let userId = sessionStorage.getItem('userId')
+    let userName = sessionStorage.getItem('userName')
+    //since the vote function have done use random value
+    let random = Math.floor(Math.random() * 2);
+    let voteResult = (random == 0) ? true : false;
+    //for test propose
+    let payload={}
+    if (i == 0) {
+        payload = {
+            name: userName,
+            id: userId,
+            vote: voteResult,
+            storyTeller: true
         }
-)
+        i++;
+    } else {
+        payload = {
+            name: userName,
+            id: userId,
+            vote: voteResult,
+            storyTeller: false
+        }
+    }
+    console.log(payload);
+    console.log(i);
+    //send the vote data to the server
+    socket.emit('send-result', payload)
+})
+
+socket.on('join', data => {
+    console.log('You have conncted to Room: ' + data);
+ })
+
+
+socket.on('addplayer', message => {
+    console.log(message);
+ })
+
+
+joinRoom.addEventListener('click',()=>{
+    const playerName = document.getElementById('inputName').value;   
+    let payload = {'playerName': playerName}
+    socket.emit('join', payload)
+})
--- services/socketServices.js
+++ services/socketServices.js
@@ -1,6 +1,9 @@
 const cal = require('./calculatorService')
 const userArray = require('./MongoService').userIdArray
+const gameController = require('../controllers/gameController');
+
 let resultArray = []
+var rooms = []
 //started the socket io
 const socketIo = (io) => {
     //while a user connected 
@@ -30,66 +33,68 @@ const socketIo = (io) => {
 
         io.emit('hello', 'hello')
 
+        socket.on('join', function (player) {
+            roomNames = Object.keys(socket.rooms);
+            console.log('Rooms: ' + roomNames)
 
 
-    })
+            socket.username = player.playerName;
+            numRooms = rooms.length;
+            if (numRooms == 0) {
+                let roomId = 0;
+                if (Object.keys(socket.rooms).length > 0) { // check the default room
+                    roomId = Object.keys(socket.rooms)[0];
+                } else
+                    roomId = 'room' + (numRooms + 1);
 
+                gameController.createRoom(roomId, function (newRoom) { // create a room object.
+                    socket.room = roomId;
+                    gameController.addPlayerToRoom(newRoom.roomId, player.playerName, socket, function (newplayer) {
+                        newRoom.players.push(newplayer);
+                        rooms.push(newRoom);
+                        socket.join(socket.room);
+                        socket.emit('join', socket.room);
+                        socket.to(socket.room).emit('addplayer', 'New player ' + player.playerName + ' joined'); // infrom others on the room 
 
-
-}
-
-socket.on('join', function (player) {
-roomNames = Object.keys(socket.rooms);
-console.log('Rooms: ' + roomNames)
-
-
-socket.username = player.playerName;
-numRooms = rooms.length;
-if (numRooms == 0) {
-    let roomId = 0;
-    if (Object.keys(socket.rooms).length > 0) { // check the default room
-        roomId = Object.keys(socket.rooms)[0];
-    } else
-        roomId = 'room' + (numRooms + 1);
-
-    gameController.createRoom(roomId, function (newRoom) { // create a room object.
-        socket.room = roomId;
-        gameController.addPlayerToRoom(newRoom.roomId, player.playerName, socket, function (newplayer) {
-            newRoom.players.push(newplayer);
-            rooms.push(newRoom);
-            socket.join(socket.room);
-            socket.emit('join', socket.room);
-            socket.to(socket.room).emit('addplayer', 'New player ' + player.playerName + ' joined'); // infrom others on the room 
-
-        })
-    })
-} else { // try to add player to the last created room
-    let currentRoom = rooms[rooms.length - 1];
-    gameController.getNumberOfPlayers(currentRoom, function (numOfPlayers) {
-        if (numOfPlayers < 4) {
-            gameController.addPlayerToRoom(currentRoom.roomId, player.playerName, socket, function (newplayer) {
-                currentRoom.players.push(newplayer);
-                socket.room = currentRoom.roomId;
-                socket.join(socket.room);
-                socket.emit('join', socket.room);
-                socket.to(socket.room).emit('addplayer', 'New player ' + player.playerName + ' joined'); // inform others on the room                  
-            })
-        } else { // create a new room 
-            let roomId = 'room' + (numRooms + 1);
-            gameController.createRoom(roomId, function (newRoom) {
-                socket.room = roomId;
-                gameController.addPlayerToRoom(newRoom.roomId, player.playerName, socket, function (newplayer) {
-                    newRoom.players.push(newplayer);
-                    rooms.push(newRoom);
-                    socket.join(socket.room);
-                    socket.emit('join', socket.room);
-                    socket.to(socket.room).emit('addplayer', 'New player ' + player.playerName + ' joined'); // inform others on the room 
+                    })
                 })
-            })
-        }
-    });
+            } else { // try to add player to the last created room
+                let currentRoom = rooms[rooms.length - 1];
+                gameController.getNumberOfPlayers(currentRoom, function (numOfPlayers) {
+                    if (numOfPlayers < 4) {
+                        gameController.addPlayerToRoom(currentRoom.roomId, player.playerName, socket, function (newplayer) {
+                            currentRoom.players.push(newplayer);
+                            socket.room = currentRoom.roomId;
+                            socket.join(socket.room);
+                            socket.emit('join', socket.room);
+                            socket.to(socket.room).emit('addplayer', 'New player ' + player.playerName + ' joined'); // inform others on the room                  
+                        })
+                    } else { // create a new room 
+                        let roomId = 'room' + (numRooms + 1);
+                        gameController.createRoom(roomId, function (newRoom) {
+                            socket.room = roomId;
+                            gameController.addPlayerToRoom(newRoom.roomId, player.playerName, socket, function (newplayer) {
+                                newRoom.players.push(newplayer);
+                                rooms.push(newRoom);
+                                socket.join(socket.room);
+                                socket.emit('join', socket.room);
+                                socket.to(socket.room).emit('addplayer', 'New player ' + player.playerName + ' joined'); // inform others on the room 
+                            })
+                        })
+                    }
+                });
+            }
+        });
+
+
+    })
+
+
+
+
+
 }
-});
+
 
 
 module.exports = {
