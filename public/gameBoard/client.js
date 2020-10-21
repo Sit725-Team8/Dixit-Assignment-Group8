96a3c9549daf3cc8e821f5f5cb7e7461c98951a9
bjw123 <brycewilkinson43@gmail.com>
2020-10-21 13:38:38 +0900

    matchmake working on gameboard and got rid of redundancies

--- public/gameBoard/client.js
+++ public/gameBoard/client.js
@@ -1,104 +1,27 @@
-
-
-
 const socket = io.connect('http://localhost:3030')
 
-// const testButton = document.getElementById('socketButton');
-// const testInput = document.getElementById('socketInput');
-// const testOutput = document.getElementById('socketOutput');
-// const calButton = document.getElementById('calculatorBtn');
-// const joinRoom = document.getElementById('joinRoom');
+let userId = sessionStorage.getItem('userId')
+let userName = sessionStorage.getItem('userName')
+
+let payload = {
+    'senderId': userId,
+    'senderName': userName
+}
+
+//setTimeout(() => {
+$( document ).ready(function() {
+    console.log( "user name:    ",sessionStorage.getItem('userName'));
+    console.log("user id:    ", sessionStorage.getItem('userId'));
+    socket.emit('joinRoom', payload)
 
 
-// const redirect = document.getElementById('redirect')
-
-// import {
-//     userId,
-//     userName
-// } from './env.js'
-setTimeout(() => {
-    console.log(sessionStorage.getItem('userName'));
-    console.log(sessionStorage.getItem('userId'));
-}, 5000);
-
-socket.on('test-socket', data => {
-    var node = document.createElement('Li')
-    var textNode = document.createTextNode(data.story);
-    node.appendChild(textNode)
-    testOutput.appendChild(node)
-    console.log(`test `);
-})
-
-// testButton.addEventListener('click', () => {
-//     let userId = sessionStorage.getItem('userId')
-//     let userName = sessionStorage.getItem('userName')
-//     let message = testInput.value;
-//     console.log(userId, userName);
-//     console.log(message);
-//     let payload = {
-//         'story': message,
-//         'senderId': userId,
-//         'senderName': userName
-//     }
-//     socket.emit('test-socket', payload)
-// })
-
-// let i = 0;
-// calButton.addEventListener('click', () => {
-//     let userId = sessionStorage.getItem('userId')
-//     let userName = sessionStorage.getItem('userName')
-//     //since the vote function have done use random value
-//     let random = Math.floor(Math.random() * 2);
-//     let voteResult = (random == 0) ? true : false;
-//     //for test propose
-//     let payload = {}
-//     if (i == 0) {
-//         payload = {
-//             name: userName,
-//             id: userId,
-//             vote: voteResult,
-//             storyTeller: true
-//         }
-//         i++;
-//     } else {
-//         payload = {
-//             name: userName,
-//             id: userId,
-//             vote: voteResult,
-//             storyTeller: false
-//         }
-//     }
-//     console.log(payload);
-//     console.log(i);
-//     //send the vote data to the server
-//     socket.emit('send-result', payload)
-// })
 
 
-// let j = 0
-// redirect.addEventListener('click', () => {
-//     j++
-//     console.log(j);
-//     if (j === 5) {
-//         window.location.replace('./gameBoard/index.html');
-//     }
-// })
-//when join room button clicked
-// joinRoom.addEventListener('click', () => {
-//     //get the user id and name from session 
-//     let userId = sessionStorage.getItem('userId')
-//     let userName = sessionStorage.getItem('userName')
-//     let payload = {
-//         userId: userId,
-//         userName: userName
-//     }
-//     //send the user inf to the server side via socket
-//     socket.emit('joinRoom', payload)
-    
-// })
+});
+
 
 //sort the room in the session in case of use that in future
-socket.on('roomNumber', data => {
+socket.on('roomNumber', data => { //
     //set teh session 
     sessionStorage.setItem('room', data.room)
     let playerNo = data.playNo
@@ -111,7 +34,15 @@ socket.on('roomNumber', data => {
     
 })
 // //once received the start game event from server
-// socket.on('startGame', data=>{
-//     //redirect to the game board
-//     window.location.replace('./gameBoard/index.html');
-// })
+socket.on('startGame', data=>{
+    /*ready to play
+    //set cards
+    //set storyteller
+    if(storyteller == true){
+        get theme and card
+        socket.emit("storytellerChoice", {theme, card})
+    }
+
+     */
+
+ })
