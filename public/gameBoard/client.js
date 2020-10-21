13790c7bab0ce848afbeacb623167204cad2b7b4
bjw123 <brycewilkinson43@gmail.com>
2020-10-21 15:59:19 +0900

    added game logic
    code needs to be properly linked with program
    
    carefull of the if array.length == x inside socket.io and any difference in order between array and socket data

--- public/gameBoard/client.js
+++ public/gameBoard/client.js
@@ -2,7 +2,7 @@ const socket = io.connect('http://localhost:3030')
 
 let userId = sessionStorage.getItem('userId')
 let userName = sessionStorage.getItem('userName')
-
+let storytellerCard;
 let payload = {
     'senderId': userId,
     'senderName': userName
@@ -47,16 +47,58 @@ socket.on('startGame', data=>{
     $("#p1c1").src( "assets/DixitCards/", cards[0], ".PNG")
     $("#p1c2").src("assets/DixitCards/", cards[1], ".PNG")
     $("#p1c3").src("assets/DixitCards/", cards[2], ".PNG")
-    "assets/DixitCards/", cards[0], ".PNG"
+
 
     /*ready to play
     //set cards
     //set storyteller
-    if(storyteller == true){
-        get theme and card
-        socket.emit("storytellerChoice", {theme, card})
-    }
 
      */
+    if(storyteller == true){
+        let theme = prompt("enter a theme that suits one of your cards and then select the card")
+        $('#theme').text(theme)
+        //$('#playerCard').click() save card
+        //$('#selectBtn').click() -> socket.emit("storyTheme", {theme, card})
+    }
+
+
 
  })
+
+
+//when receiving info from storyteller
+let cards = []
+cards.push({storyteller, storytellerCard})
+socket.on("storyTheme", data => {
+    //*add html element to show theme to UI
+    $("#theme").text(data.theme)
+    storytellerCard= data.card
+    alert("select a card that matches the theme")
+    //$(#playercard).onclick() -> save card
+    //$(#selectBtn).onclick() -> cards.push({player, card})  ->socket.emit("similarCard", {card, player})
+})
+
+
+let guesses = []
+
+socket.on("similarCard", data => {//carefull of data order
+    cards.push(data)
+    if (cards.length == 4 && storyteller == false){
+        alert("guess which card is the storytellers")
+        //$(#choosencard).onclick() -> save card
+        //$(#guessBtn).onclick() -> guesses.push({player, card}) -> socket.emit("guessCard", {card, player})
+
+    }
+})
+
+socket.on("guessCard", data => { //careful of data order
+    guesses.push(data)
+    if(guesses.length == 3){
+        //start voting
+        //meshals .....
+    }
+
+})
+
+
+
